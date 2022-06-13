a3b455b73e325ab46ba0f0f47ff2fe1c1f6edd08
abhishekbt21 <104352282+abhishekbt21@users.noreply.github.com>
2022-06-13 11:38:13 +0530

    Add files via upload

---
+++ output ass 3.py
@@ -0,0 +1,32 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\Acer\Downloads\New folder\Assignment 3rd ==========
+Enter your number5
+101
+Enter first number: 2
+Enter sign(+, -, *, / or q for quit): +
+Next number: 5
+Enter next sign(+, -, *, / or q for quit): -
+Next number: 2
+Enter next sign(+, -, *, / or q for quit): +
+Next number: 1
+Enter next sign(+, -, *, / or q for quit): *
+Next number: 8
+Enter next sign(+, -, *, / or q for quit): *
+Next number: 7
+Enter next sign(+, -, *, / or q for quit): q
+336.0
+35
+45.0
+1256.6370614359173
+3.1622776601683795
+0.5
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+Enter number of Hydrogen Atoms: 3
+Enter number of Carbon Atoms: 4
+Enter number of Oxygen Atoms: 2
+Molecular weight of compound is: 83.06542 grams/mole
